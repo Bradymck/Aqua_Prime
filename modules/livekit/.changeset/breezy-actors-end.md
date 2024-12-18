707510d4050de2eb72d590d01286e9dbb94c2822
Bradymck <mckmuze@gmail.com>
2024-12-18 08:18:34 -0800

    chore: clean up repository and set up basic public structure

--- modules/livekit/.changeset/breezy-actors-end.md
+++
@@ -1,7 +0,0 @@
----
-"@livekit/agents": patch
-"@livekit/agents-plugin-openai": patch
-"livekit-agents-examples": patch
----
-
-fix(treewide): use newer rtc-node version
--- modules/livekit/agents/src/stt/index.ts
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
-//
-// SPDX-License-Identifier: Apache-2.0
-
-export { STT, SpeechEvent, SpeechEventType, SpeechStream, type SpeechData } from './stt.js';
-export { StreamAdapter, StreamAdapterWrapper } from './stream_adapter.js';
